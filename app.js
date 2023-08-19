62e5f58fefb6f9fe7a54e6f9da4f450da068b5d3
Shubham Mishra <21cse53@vmkvec.edu.in>
2023-08-19 21:46:07 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,89 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns=["yellow","red","purple","green"];
+
+let started = false;
+let level=0;
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+     console.log("game started");
+     started=true;
+     levelUp();
+    }   
+});
+  
+
+ function gameFlase(btn){
+    btn.classList.add("flash"); 
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+
+ }
+
+ function userFlase(btn){
+    btn.classList.add("userflash"); 
+    setTimeout(function(){
+        btn.classList.remove(" userflash");
+    },250);
+
+  }
+function levelUp () {
+    userSeq=[];
+    level++;
+    h2.innerText=`level${level}`;
+    //random btn choose
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`);
+    // console.log(randIdx);
+    // console.log(randColor);
+    // console.log(randBtn);
+    gameSeq.push(randColor);
+    
+    gameFlase(randBtn);
+}
+function checkAns(idx){
+    // console.log("curr level :" ,level);
+     
+    if(userSeq[idx]===gameSeq[idx]){
+         if(userSeq.length==gameSeq.length){
+            setTimeout(levelUp,1000);
+            
+         }
+    }else{
+        h2.innerHTML=`Game over! your score was <b>${level}</b><br> press any key to start`; 
+        document.querySelector("body").style.background="red";
+        setTimeout(function(){
+            document.querySelector("body").style.background="white";
+        },150);
+        reset();
+    }
+
+}
+
+  function btnPress () {
+ //   console.log(this);
+     let btn=this;
+    // btnFlase(btn);
+     userFlase(btn);
+
+    userColor=btn.getAttribute("id");
+    userSeq.push(userColor); 
+    checkAns(userSeq.lenght-1);
+
+ }
+
+ let allBtns = document.querySelectorAll(".btn");
+ for(btn of allBtns){
+     btn.addEventListener("click",btnPress)
+ }
+function reset(){
+    started=false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
